4f8f9f874ed3afe271e0bc42e56877f943e52d74
Littl3S3al <lauseal@gmail.com>
2020-03-31 15:52:52 +0200

    heroku changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.get('' , (req, res) => {
     res.send('Ctrl Copy website coming soon');;
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
